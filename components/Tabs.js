d1368c80f8e64076f4060be1bd44618893a88cd2
Sathya Ganesan <sathyaganesan.b2w@gmail.com>
2020-08-16 22:23:46 -0400

    Tabs file is done

--- components/Tabs.js
+++ components/Tabs.js
@@ -10,10 +10,11 @@ axios
     tabArray = res.data.topics;
     console.log(tabArray)
 
-    tabArray.forEach((item) => {
-        tabs.textContent = item;
-        topics.appendChild(tabs);
-    });    
+    elemt1.textContent = tabArray[0];
+    elemt2.textContent = tabArray[1];
+    elemt3.textContent = tabArray[2];
+    elemt4.textContent = tabArray[3];
+    elemt5.textContent = tabArray[4];
 
 })
 .catch((err) => {
@@ -27,10 +28,23 @@ axios
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
-const topics = document.querySelector('.topics');
-const tabs = document.createElement('div');
+const topicsEl = document.querySelector('.topics');
+const elemt1 = document.createElement('div');
+const elemt2 = document.createElement('div');
+const elemt3 = document.createElement('div');
+const elemt4 = document.createElement('div');
+const elemt5 = document.createElement('div');
 
+elemt1.classList.add('tab');
+elemt2.classList.add('tab');
+elemt3.classList.add('tab');
+elemt4.classList.add('tab');
+elemt5.classList.add('tab');
 
+topicsEl.appendChild(elemt1);
+topicsEl.appendChild(elemt2);
+topicsEl.appendChild(elemt3);
+topicsEl.appendChild(elemt4);
+topicsEl.appendChild(elemt5);
 
-//
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
